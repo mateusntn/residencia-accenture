b3772fdfc31f655eb121e30610999c0d6c7c164f
mateusntn <mateusantoniodev@gmail.com>
2021-05-21 03:27:22 -0300

    ALF(5.5): Creates home structure  and dashboard component

--- alokarFrontend/src/app/app-routing.module.ts
+++ alokarFrontend/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ProjectCrudComponent } from './views/project-crud/project-crud.component'
+import { ProjectCrudComponent } from './views/project-crud/project-crud.component';
+import { HomeComponent } from './views/home/home.component'
 
 const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
   {
     path: "projects",
     component: ProjectCrudComponent
