4c4c8a075f989c2ea539738db8a880fff1d32450
AlandaAquino <alandaoaquino@fac.pe.senac.br>
2021-05-24 23:29:53 -0300

    ALB(5.1) Create relationships between all the models

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+projeto_habilidade = db.Table('projetos_habilidades',
+    db.Column('projetos.id', db.Integer, db.ForeignKey('projetos.id')),
+    db.Column('habilidades.id', db.Integer, db.ForeignKey('habilidades.id'))
+)
+
 class Projeto(db.Model):
     __tablename__ = "projetos"
     id = db.Column(db.Integer, primary_key=True)
@@ -29,6 +34,9 @@ class Projeto(db.Model):
     status = db.Column(db.String(45), nullable=True, index=True)
     seatCharge = db.Column(db.Float, nullable=True, index=True)
     nomeEmpresa = db.Column(db.String(45), nullable=True, index=True)
+    
+    projetos_habilidades = db.relationship('Habilidade', secondary='projetos_habilidades', lazy='dynamic')
+    funcionarios = db.relationship("Funcionario", secondary="funcionarios_projetos")
 
 
 class Habilidade(db.Model):
@@ -36,11 +44,13 @@ class Habilidade(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(45), nullable=False, index=True)
 
+    funcionarios = db.relationship("Funcionario", secondary="funcionarios_habilidades")
 
 class Cargo(db.Model):
     __tablename__ = "cargos" 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(45), nullable=False, index=True)
+
     funcionarios = db.relationship('Funcionario', backref='cargos')
 
 
@@ -54,6 +64,10 @@ class Funcionario(db.Model):
     custoHora_overtime = db.Column(db.String(45), nullable=True)
     cargo_id = db.Column(db.Integer, db.ForeignKey('cargos.id'), nullable=False)
 
+    habilidades = db.relationship("Habilidade", secondary="funcionarios_habilidades")
+    projetos = db.relationship("Projeto", secondary="funcionarios_projetos")
+    usuarios = db.relationship("Usuario", back_populates="funcionarios")
+
 
 
 class Usuario(db.Model):
@@ -62,16 +76,32 @@ class Usuario(db.Model):
     nome = db.Column(db.String(45), nullable=False, index=True)
     email = db.Column(db.String(45), nullable=True, index=True)
     senha = db.Column(db.String(250), nullable=True)
-    funcionarios_id = db.Column(db.Integer, nullable=False)
+    funcionarios_id = db.Column(db.Integer, db.ForeignKey('funcionarios.id'))
     funcionarios_cargo_id = db.Column(db.Integer, nullable=False)
+
+    funcionarios = db.relationship("Funcionario", back_populates="usuarios")
 
 
 class Funcionario_habilidade(db.Model):
     __tablename__ = "funcionarios_habilidades" 
-    funcionarios_id = db.Column(db.Integer, foreign_key=True)
-    habilidades_id = db.Column(db.Integer, foreign_key=True)
-    tempoExperiencia = db.Column(db.Integer, nullable=False)
+    funcionarios_id = db.Column(db.Integer, db.ForeignKey('funcionarios.id'), primary_key=True)
+    habilidades_id = db.Column(db.Integer, db.ForeignKey('habilidades.id'), primary_key=True)
+    tempoExperiencia = db.Column(db.String(45), nullable=False)
+    
+    habilidades = db.relationship("Habilidade", backref=backref("funcionarios_habilidades", cascade="all, delete-orphan"))
+    funcionarios = db.relationship("Funcionario", backref=backref("funcionarios_habilidades", cascade="all, delete-orphan"))
 
+
+
+class Funcionario_projeto(db.Model):
+    __tablename__ = "funcionarios_projetos" 
+    funcionarios_id = db.Column(db.Integer, db.ForeignKey('funcionarios.id'), primary_key=True)
+    projetos_id = db.Column(db.Integer, db.ForeignKey('projetos.id'), primary_key=True)
+    overtime = db.Column(db.Integer, nullable=True)
+    funcao = db.Column(db.Integer, nullable=True)
+    
+    projetos = db.relationship("Projeto", backref=backref("funcionarios_projetos", cascade="all, delete-orphan"))
+    funcionarios = db.relationship("Funcionario", backref=backref("funcionarios_projetos", cascade="all, delete-orphan"))
 
 def __str__(self):
     return self.name
