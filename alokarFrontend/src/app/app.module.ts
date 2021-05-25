584d550e35ebfd7602b38b2e6d4d3a88f398b3ce
mateusntn <mateusantoniodev@gmail.com>
2021-05-25 00:31:25 -0300

    ALF(6.2): Creates project-create component

--- alokarFrontend/src/app/app.module.ts
+++ alokarFrontend/src/app/app.module.ts
@@ -4,18 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { NavComponent } from './components/template/nav/nav.component';
-
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+
 import { ProjectCrudComponent } from './views/project-crud/project-crud.component';
 import { ProjectReadComponent } from './components/project/project-read/project-read.component';
 import { HomeComponent } from './views/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProjectCreateComponent } from './components/project/project-create/project-create.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HeaderComponent } from './components/template/header/header.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +28,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     ProjectCrudComponent,
     ProjectReadComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProjectCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +38,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
