957853589812cd54f79b957050283ec9bd3e4074
mateusntn <mateusantoniodev@gmail.com>
2021-06-01 01:14:29 -0300

    ALF(8.7): Creates project-allocation view

---
+++ alokarFrontend/src/app/views/project-allocation/project-allocation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-allocation',
+  templateUrl: './project-allocation.component.html',
+  styleUrls: ['./project-allocation.component.css']
+})
+export class ProjectAllocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
